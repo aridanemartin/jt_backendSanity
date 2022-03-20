eb7e5f322790b9977533c7a0ba69db8ee0604d29
Aridane Martín <aridanemartin@gmail.com>
2022-03-20 19:38:35 +0000

    added confirmation page

---
+++ schemas/confirmation.js
@@ -0,0 +1,87 @@
+export default {
+  name: "confirmation",
+  title: "Confirmación de Cita",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "form1Title",
+      title: "Formulario 1 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form1Link",
+      title: "Formulario 1 - Link",
+      type: "string",
+    },
+    {
+      name: "form2Title",
+      title: "Formulario 2 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form2Link",
+      title: "Formulario 2 - Link",
+      type: "string",
+    },
+    {
+      name: "form3Title",
+      title: "Formulario 3 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form3Link",
+      title: "Formulario 3 - Link",
+      type: "string",
+    },
+    {
+      name: "form4Title",
+      title: "Formulario 4 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form4Link",
+      title: "Formulario 4 - Link",
+      type: "string",
+    },
+    {
+      name: "form5Title",
+      title: "Formulario 5 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form5Link",
+      title: "Formulario 5 - Link",
+      type: "string",
+    },
+    {
+      name: "form6Title",
+      title: "Formulario 6 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form6Link",
+      title: "Formulario 6 - Link",
+      type: "string",
+    },
+    {
+      name: "form7Title",
+      title: "Formulario 7 - Nombre",
+      type: "string",
+    },
+    {
+      name: "form7Link",
+      title: "Formulario 7 - Link",
+      type: "string",
+    },
+  ],
+  preview: {
+    select: {
+      title: "name",
+    },
+  },
+};
